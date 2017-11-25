65ddc2c20d881993d8024756d5a80439a219c9b9
Chalapathi <princechalapathi16@gmail.com>
2017-11-25 15:14:34 +0000

    first commit

---
+++ CP8298_I00-vendor-blobs.mk
@@ -0,0 +1,17 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/app/mcRegistry,system/app/mcRegistry)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/CP8298_I00/priv-app/CameraNextModv7,system/priv-app/CameraNextModv7)
